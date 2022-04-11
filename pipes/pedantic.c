26834a1daffc6695da52800fc0651c9a731ff391
Batuhan Keskintas <batuhankeskintas@Batuhans-MacBook-Pro.local>
2022-04-11 17:41:30 +0300

    11041741

--- pipes/pedantic.c
+++ pipes/pedantic.c
@@ -6,7 +6,7 @@
 /*   By: bkeskint <bkeskint@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 20:44:55 by bkeskint          #+#    #+#             */
-/*   Updated: 2022/01/24 21:09:29 by bkeskint         ###   ########.fr       */
+/*   Updated: 2022/02/11 22:48:16 by bkeskint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ int	main(int argc, char *argv[])
 	int		fds[2];										//an array that will hold 2 file descriptors
 	pipe(fds);											//populates fds with 2 file descriptors
 	pid_t	pid = fork();								//create child process that is a clone of the parent
-
 	if (pid == 0)										//"0" is for child process
 	{
 		dup2(fds[0], STDIN_FILENO);						//fds[0] (the read end of pipe) donates its data to file descriptor 0
 		close(fds[0]);									//file descriptor no longer needs in the child since stdin is a copy
 		close(fds[1]);									//file descriptor unused in child
 		char	*argv[] = {(char *)"sort", NULL}; 		//create argument vector
-		if (execvp(argv[0], argv)< 0)					//run sort command - exit if something wrong
+		if (execvp(argv[0], argv) < 0)					//run sort command - exit if something wrong
 			exit(0);
 	}													//after this point we are moving with parent process
 	close(fds[0]);										//file descriptor unused in parent
